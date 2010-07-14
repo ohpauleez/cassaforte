e8130edecf2c915b1b6725657d92e8993fa9df60
Paul deGrandis <paul.degrandis@gmail.com>
2010-07-14 18:26:59 -0400

    Added a map of consistency levels built via reflection, added comments, and am attempting to overide the proxy-client macro (unsuccessfully)

--- src/cassaforte.clj
+++ src/cassaforte.clj
@@ -1,5 +1,6 @@
 (ns cassaforte
-  "An extendable Apache Cassandra client for Clojure"
+  #^{:author "Paul deGrandis"
+     :doc "An extendable Apache Cassandra client for Clojure"}
   (:import
     (org.apache.cassandra.thrift Cassandra$Client ColumnPath)
     (org.apache.thrift.transport TSocket)
@@ -7,21 +8,42 @@
 
 (defn col [cf c] (let [cp (ColumnPath. cf)]
                 (.setColumn cp (.getBytes c)) cp))
-(def cl-all org.apache.cassandra.thrift.ConsistencyLevel/ALL)
+
+(def levels (let [cls (org.apache.cassandra.thrift.ConsistencyLevel/values)
+                  words (map #(.toString %)  cls)]
+              (zipmap (map #(keyword (.toLowerCase %)) words) (map #(org.apache.cassandra.thrift.ConsistencyLevel/valueOf %) words))))
 
 (defn connect-to
+  "A function that create a client connected to a Cassandra Thrift interface
+  Arguments:
+    host - a String, the host you're trying to reach
+    port - an int, the port number of the Thrift interface
+  Returns:
+    client - a Cassandra$Client object
+  Example:
+    (def raw-client (connect-to 'localhost' 9160))
   "
-  Connect to a Cassandra Thrift interface
-  "
-  (^Cassandra$Client [^String host ^int port]
+  ^Cassandra$Client [^String host ^int port]
     (let [tsock (TSocket. host port)
           tprot (TBinaryProtocol. tsock)
           client (do (.open tsock)
                  (Cassandra$Client. tprot))]
-      client)))
+      client))
 
-(defmacro client
-  "
+(defmacro proxy-client
+  "A macro that returns a function,
+  that passes abritrary methods (specificed as Strings) and aruments to a client.
+  Arguments:
+    host - a String, the host you're trying to reach
+    port - an int, the port number of the Thrift interface
+  Returns:
+     - a function, which you pass Cassandra$Client methods (specified as Strings)
+        and arguments to
+  Example:
+    (def cas (client 'localhost' 9160))
+    (cas 'get' 'my-keyspace' 'my-key' (col 'my-column-family' 'my-column') (levels :all))
+  NOTES:
+    THIS DOES NOT CURRENTLY WORK
   "
   [^String host ^int port]
   `(let [client# (connect-to ~host ~port)]
@@ -32,3 +54,17 @@
          #_(. client# m# a#)
          #_(.m# client# a#)))))
 
+(defmacro client2
+  "
+  "
+  ([]
+   (client2 "localhost" 9160))
+  ([^String host ^Integer port]
+   (client2 (connect-to host port)))
+  ([^Cassandra$Client cclient]
+  `(fn [method# & args#]
+     (let [m# (.getMethod Cassandra$Client method# (into-array Class (map type args#)))
+           a# (into-array Object args#)]
+       (println ~cclient m# args#)
+       #_(. ~cclient m# a#)
+       #_(.m# ~cclient a#)))))
